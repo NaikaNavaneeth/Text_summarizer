4edfd728b036e0c2f43fd1c29b5af137efa890a9
NaikaNavaneeth <164477569+NaikaNavaneeth@users.noreply.github.com>
2025-05-10 22:51:46 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,9 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 8089,
   },
   plugins: [
-    react({
-      jsx: 'react-jsx', 
-    }),
+    react(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
